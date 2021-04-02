1adae4c70b429aed18a6dabae3fa60805210ea70
heba <hebaayman77177>
2021-04-02 10:53:43 +0200

    create create note page

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Notes from "./pages/Notes";
+import Create from "./pages/Create";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core";
+import { deepOrange, green } from "@material-ui/core/colors";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: deepOrange,
+    secondary: green,
+  },
+  typography: {
+    fontFamily: "Quicksand",
+    fontWeightLight: 400,
+    fontWeightRegular: 500,
+    fontWeightMedium: 600,
+    fontWeightBold: 700,
+  },
+});
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Notes />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
+  );
+}
+
+export default App;
