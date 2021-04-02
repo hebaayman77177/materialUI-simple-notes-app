1467a54d00f47441e744d27b3dee5f3bcdf26681
heba <hebaayman77177>
2021-04-02 18:49:17 +0200

    add layout and make notes pages

--- src/App.js
+++ src/App.js
@@ -3,19 +3,22 @@ import Notes from "./pages/Notes";
 import Create from "./pages/Create";
 import { ThemeProvider } from "@material-ui/core";
 import theme from "./theme";
+import Layout from "./components/Layout";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <Router>
-        <Switch>
-          <Route exact path="/">
-            <Notes />
-          </Route>
-          <Route path="/create">
-            <Create />
-          </Route>
-        </Switch>
+        <Layout>
+          <Switch>
+            <Route exact path="/">
+              <Notes />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+          </Switch>
+        </Layout>
       </Router>
     </ThemeProvider>
   );
